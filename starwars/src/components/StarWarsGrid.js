2ef5c3e54382a04dcb0653699305c9ce803cbede
brittanymae01 <brittanymae01>
2019-11-08 09:05:09 -0800

    downloaded dependencies

--- starwars/src/components/StarWarsGrid.js
+++ starwars/src/components/StarWarsGrid.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { setState, useEffect } from 'react'
 import StarWarsCard from './StarWarsCard'
 
 function StarWarsGrid() {
+    const [starCharacter, setStarCharacter] = setState([])
+
+    useEffect(() => {
+        axios.get('https://swapi.co/api/people/').then(response => {
+            console.log(response.data.results)
+            // setStarCharacter(response.data.results)
+        }).catch(error => {
+            console.log('the data was not returned', error)
+        })
+    }, [])
     return (
         <div>
             <StarWarsCard />
